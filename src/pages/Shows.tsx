9693beeab7d17d37fd7d2ae9a5e2ceb2f96ddc15
kudrekode <j.a.d.crick@gmail.com>
2025-03-03 09:38:00 +0000

    removed gig

--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -14,12 +14,12 @@ const Shows: React.FC = () => {
         <h1>UPCOMING SHOWS</h1>
         <div className="shows-container">
             <div className="show-list">
-                <h3>15th March</h3>
-                <a href={"https://link.dice.fm/Z2c56552e461"}>The Finsbury - The Amber Bugs Launch Party</a>
-                {/*<p>Free Show</p>*/}
-                <a href={"https://link.dice.fm/Z2c56552e461"}>
-                    <img src="assets/amberbugs.png" alt="Amber Bugs Release Party" className="gigs-images"></img>
-                </a>
+                {/*<h3>15th March</h3>*/}
+                {/*<a href={"https://link.dice.fm/Z2c56552e461"}>The Finsbury - The Amber Bugs Launch Party</a>*/}
+                {/*/!*<p>Free Show</p>*!/*/}
+                {/*<a href={"https://link.dice.fm/Z2c56552e461"}>*/}
+                {/*    <img src="assets/amberbugs.png" alt="Amber Bugs Release Party" className="gigs-images"></img>*/}
+                {/*</a>*/}
                 <h3>20th March</h3>
                 <a href={"https://skiddle.com/e/40647411/?skcampaign=Kudre"}>The Engine Rooms - Headline Show</a>
                 <a href={"https://skiddle.com/e/40647411/?skcampaign=Kudre"}>
